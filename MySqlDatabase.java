4de4d8010ec9bd18d71704a949c602faa1ad77c4
Onat Karabulut <onatk12@gmail.com>
2022-07-20 16:06:42 +0300

    files added

---
+++ MySqlDatabase.java
@@ -0,0 +1,21 @@
+public class MySqlDatabase implements IDatabase {
+   @Override 
+   public void add(){
+      System.out.println("add the Mysql database");
+   }
+
+   @Override 
+   public void delete(){
+      System.out.println("delete the Mysql database");
+   }
+
+   @Override
+   public void get(){
+      System.out.println("get the Mysql database");
+   }
+
+   @Override
+   public void update(){
+      System.out.println("update the Mysql database");
+   }
+}
